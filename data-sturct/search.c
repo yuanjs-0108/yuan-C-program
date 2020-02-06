08c51243d071364a0753d1f96d957cb2985fac63
yuan_js <13767589902@163.com>
2020-02-06 13:55:00 +0800

    enter

---
+++ data-sturct/search.c
@@ -0,0 +1,48 @@
+﻿#include <define.h>
+//基础的查找算法
+
+
+/*顺序查找
+使用顺序表或链表实现。
+从头到尾依次查找
+*/
+//顺序表的查找
+int search(sqList l,elemtype x){
+    for (int i = 0; i < l.len-1; i++)
+        if(l.data[i]==x)
+            return i;
+    return -1;    
+}
+//链表的查找
+int search(snode *head,elemtype x){
+    snode *p=head;
+    int cnt=0;
+    while (p)
+    {
+        cnt++;
+        if(p->data==x) break;
+        p=p->next;
+    }
+    return cnt;    
+}
+
+/*二分查找
+给定数据必须有序
+首先与中间元素进行比较
+大-->移动左边界=mid+1
+小-->移动右边界=mid-1
+继续以上操作
+*/
+
+int binaryser(sqList l,elemtype x){
+    int left=0,right=l.len-1,mid;
+    while (left<right)
+    {
+        mid=(left+right)/2;
+        if(l.data[mid]<x) left=mid+1;
+        else if(l.data[mid]>x) right=mid-1;
+        else return mid;
+    }
+    return -1;
+    
+}
---
+++ data-sturct/sort.c
@@ -0,0 +1,53 @@
+﻿#include <define.h>
+//基础的排序算法
+
+//直接插入排序
+void inshort(sqList *l){
+    int i,j;
+    for ( i = 2; i < l->len; i++)
+    {
+        l->data[0]=l->data[i];
+        j=i-1;
+        while (l->data[0]<l->data[j])
+        {
+            l->data[j+1]=l->data[j];
+            j--;
+        }
+        l->data[j+1]=l->data[0];
+    }
+    
+}
+
+//简单选择排序
+void esshort(sqList *l){
+    int i,j,k;
+    for (i = 0; i < l->len-1; i++)
+    {
+        k=i;
+        for (j = i+1; i < l->len; j++)
+            if (l->data[k]>l->data[j])
+                k=j;     //找到最小数的下标
+        int t=l->data[k];
+        l->data[k]=l->data[i];
+        l->data[i]=t;
+    }
+}
+
+//冒泡排序
+void bubshort(sqList *l){
+    int i,j,flag;
+    for ( i = 1; i < l->len; i++)//趟数
+    {
+        flag=0;//0表示未交换，1表示交换
+        for (j = 0; j < l->len-1; j++)
+            if(l->data[j]<l->data[j+1]){
+                int t=l->data[j];
+                l->data[j]=l->data[j+1];
+                l->data[j+1]=t;
+                flag=1;
+            }
+        if (flag==0) break;
+    }
+    
+}
+
